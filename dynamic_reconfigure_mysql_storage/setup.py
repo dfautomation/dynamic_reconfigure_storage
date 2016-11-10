4a6b72a77fe7944e733c2999a27a1adb538b71af
Patrick Chin <patrickcjh@gmail.com>
2016-11-10 11:34:14 +0800

    Fix issue with distutils setup file.

--- dynamic_reconfigure_mysql_storage/setup.py
+++ dynamic_reconfigure_mysql_storage/setup.py
@@ -6,7 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['dynamic_reconfigure_mysql_storage'],
     package_dir={'': 'src'},
-    requires=['dynamic_reconfigure_storage_utils', 'MySQL-python', 'rospy', 'SQLAlchemy']
+    requires=['dynamic_reconfigure_storage_utils', 'rospy', 'SQLAlchemy'],
+    install_requires=['MySQL-python']
 )
 
 setup(**d)
